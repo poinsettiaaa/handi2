41c9c07c1bde65b137f1050aefcfde4e2a2cb45a
handi <poinsettiaaa@naver.com>
2019-12-01 17:16:02 +0900

    코틀린

---
+++ app/src/main/java/com/example/handi2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.handi2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.handi2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val user = User("이름 노출", "주소 노출")
+        binding.setVariable(BR.user, user)
+        binding.executePendingBindings()
+    }
+}
---
+++ app/src/main/java/com/example/handi2/User.kt
@@ -0,0 +1,6 @@
+package com.example.handi2
+
+data class User(
+    var name: String = ""
+    , var address: String = ""
+)
